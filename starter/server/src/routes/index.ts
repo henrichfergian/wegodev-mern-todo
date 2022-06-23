b88c91fa739894c569e192bbf2e779a5513c51f3
Henrich Fergian <henrichfergian1@gmail.com>
2022-06-24 01:51:14 +0700

    create router and getTodos controller

---
+++ starter/server/src/routes/index.ts
@@ -0,0 +1,11 @@
+import { Router } from 'express';
+
+const router = Router();
+
+router.get('/api/todos', getTodos);
+router.get('/api/todos/:id', getTodo);
+router.post('/api/todos', addTodo);
+router.put('/api/update-todo/:id', updateTodo);
+router.delete('/api/remove-todo/:id', removeTodo);
+
+export default router;
